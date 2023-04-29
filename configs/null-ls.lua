ccf5be32dff2920b860eabc88ec2bde804ce8a76
Bruno Krugel <brunokrugel@gmail.com>
2023-04-28 22:23:11 -0300

    Update null-ls.lua

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -14,7 +14,7 @@ local sources = {
   b.code_actions.gitsigns,
   -- Go
   -- b.formatting.gofmt,
-  -- b.formatting.gofumpt,
+  b.formatting.gofumpt,
   -- b.formatting.goimports,
   -- b.formatting.goimports_reviser,
   -- b.diagnostics.golangci_lint,
@@ -55,7 +55,22 @@ local sources = {
 -- table.insert(sources, golangci_lint)
 -- table.insert(sources, gotest_codeaction)
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
   debug = true,
   sources = sources,
+
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --       vim.api.nvim_create_autocmd("BufWritePre", {
+  --           group = augroup,
+  --           buffer = bufnr,
+  --           callback = function()
+  --               -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+  --               vim.lsp.buf.format({ bufnr = bufnr })
+  --           end,
+  --       })
+  --   end
+  -- end,
 }
