d1e29fff3d71e5eea2eb5c34169321600a6e80dd
Bruno Krugel <brunokrugel@gmail.com>
2023-04-28 21:41:19 -0300

    Update null-ls.lua

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -7,8 +7,6 @@ end
 local b = null_ls.builtins
 
 local sources = {
-
-  -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
   b.formatting.clang_format,
