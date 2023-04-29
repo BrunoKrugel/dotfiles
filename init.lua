68b08444a2d790c04b1f6852e36670b6874fc3e7
Bruno Krugel <brunokrugel@gmail.com>
2023-04-28 23:35:06 -0300

    remove  vt

--- init.lua
+++ init.lua
@@ -22,13 +22,10 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
 -- disable underline in diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = false,
-  virtual_text = {
-    spacing = 4,
-    prefix = "",
-  },
   signs = true,
   update_in_insert = false,
 })
