f0c497afa66afec071ae4da5e62a027f2a649bbe
Bruno Krugel <brunokrugel@gmail.com>
2023-05-11 14:22:41 -0300

    remove cursor hl

--- init.lua
+++ init.lua
@@ -59,24 +59,24 @@ autocmd("BufWritePre", {
   end,
 })
 
-autocmd({ "ModeChanged" }, {
-  callback = function()
-    local current_mode = vim.fn.mode()
-    if current_mode == "n" then
-      vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#f8f8f2" })
-      -- vim.fn.sign_define("smoothcursor", { text = "" })
-    elseif current_mode == "v" then
-      vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#62d6e8" })
-      -- vim.fn.sign_define("smoothcursor", { text = "" })
-    elseif current_mode == "V" then
-      vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#62d6e8" })
-      -- vim.fn.sign_define("smoothcursor", { text = "" })
-    elseif current_mode == "�" then
-      vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#62d6e8" })
-      -- vim.fn.sign_define("smoothcursor", { text = "" })
-    elseif current_mode == "i" then
-      vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#50fa7b" })
-      -- vim.fn.sign_define("smoothcursor", { text = "" })
-    end
-  end,
-})
+-- autocmd({ "ModeChanged" }, {
+--   callback = function()
+--     local current_mode = vim.fn.mode()
+--     if current_mode == "n" then
+--       vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#f8f8f2" })
+--       -- vim.fn.sign_define("smoothcursor", { text = "" })
+--     elseif current_mode == "v" then
+--       vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#62d6e8" })
+--       -- vim.fn.sign_define("smoothcursor", { text = "" })
+--     elseif current_mode == "V" then
+--       vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#62d6e8" })
+--       -- vim.fn.sign_define("smoothcursor", { text = "" })
+--     elseif current_mode == "�" then
+--       vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#62d6e8" })
+--       -- vim.fn.sign_define("smoothcursor", { text = "" })
+--     elseif current_mode == "i" then
+--       vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#50fa7b" })
+--       -- vim.fn.sign_define("smoothcursor", { text = "" })
+--     end
+--   end,
+-- })
