fe8487ab1a8c8713152a2b02ddf4fd0d40f05a5b
Bruno Krugel <brunokrugel@gmail.com>
2023-05-14 17:16:15 -0300

    Update gpt.lua

--- configs/gpt.lua
+++ configs/gpt.lua
@@ -1,68 +1,10 @@
-local present, gpt = pcall(require, "gpt")
+local present, gpt = pcall(require, "chatgpt")
 
 if not present then
   return
 end
 
-local config = {
-  welcome_message = WELCOME_MESSAGE, -- set to "" if you don't like the fancy godot robot
-  loading_text = "loading",
-  question_sign = "󱍋",            -- you can use emoji if you want e.g. 🙂
-  answer_sign = "󱜚",              --
-  max_line_length = 120,
-  yank_register = "+",
-  chat_layout = {
-    relative = "editor",
-    position = "50%",
-    size = {
-      height = "80%",
-      width = "80%",
-    },
-  },
-  settings_window = {
-    border = {
-      style = "rounded",
-      text = {
-        top = " Settings ",
-      },
-    },
-  },
-  chat_window = {
-    filetype = "chatgpt",
-    border = {
-      highlight = "FloatBorder",
-      style = "rounded",
-      text = {
-        top = " ChatGPT ",
-      },
-    },
-  },
-  chat_input = {
-    prompt = "  ",
-    border = {
-      highlight = "FloatBorder",
-      style = "rounded",
-      text = {
-        top_align = "center",
-        top = " Prompt ",
-      },
-    },
-  },
-  openai_params = {
-    model = "text-davinci-003",
-    frequency_penalty = 0,
-    presence_penalty = 0,
-    max_tokens = 300,
-    temperature = 0,
-    top_p = 1,
-    n = 1,
-  },
-  openai_edit_params = {
-    model = "code-davinci-edit-001",
-    temperature = 0,
-    top_p = 1,
-    n = 1,
-  },
+gpt.setup {
   keymaps = {
     close = { "<C-c>", "<Esc>" },
     yank_last = "<C-y>",
@@ -73,4 +15,4 @@ local config = {
     cycle_windows = "<Tab>",
   },
 }
-require("chatgpt").setup(config)
+
