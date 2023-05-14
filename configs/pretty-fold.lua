9f9b92fc7d39d37d6f20f953e4d7055a3e64eb5f
Bruno Krugel <brunokrugel@gmail.com>
2023-05-14 12:02:37 -0300

    move icons

--- configs/pretty-fold.lua
+++ configs/pretty-fold.lua
@@ -6,23 +6,23 @@ end
 
 prettyFold.setup {
   keep_indentation = false,
-  fill_char = "━",
+  fill_char = " ",
   sections = {
     left = {
-      "━ ",
+      -- "━ ",
       function()
         return string.rep("*", vim.v.foldlevel)
       end,
-      " ━┫",
+      -- " ━┫",
       "content",
-      "┣",
+      -- "┣",
     },
     right = {
-      "┫ ",
+      -- "┫ ",
       "number_of_folded_lines",
       ": ",
       "percentage",
-      " ┣━━",
+      -- " ┣━━",
     },
   },
 }
