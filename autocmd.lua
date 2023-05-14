d98163dcd0c59af2d84445925a9c3a7eb22bde43
Bruno Krugel <brunokrugel@gmail.com>
2023-05-14 17:16:08 -0300

    yank

--- autocmd.lua
+++ autocmd.lua
@@ -43,3 +43,13 @@ autocmd("BufWritePre", {
     vim.lsp.buf.format { async = false }
   end,
 })
+
+autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = "IncSearch",
+      timeout = 100,
+    }
+  end,
+})
