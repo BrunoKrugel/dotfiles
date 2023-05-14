6ab112650e8c2d77fb686ff0313e3390574a831f
Bruno Krugel <brunokrugel@gmail.com>
2023-05-14 13:56:13 -0300

    ufo

--- init.lua
+++ init.lua
@@ -33,10 +33,11 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 vim.g.bookmark_sign = ""
 vim.g.bookmark_highlight = "DevIconErb"
 
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-vim.o.foldcolumn = "0"
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 require("custom.autocmd")
 require("custom.usrcmd")
