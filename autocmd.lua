8365ca98be6ceee3b44bbca038e8d3131601450c
Bruno Krugel <brunokrugel@gmail.com>
2023-05-25 14:55:13 -0300

    Update autocmd.lua

--- autocmd.lua
+++ autocmd.lua
@@ -29,6 +29,21 @@ autocmd("VimResized", {
   command = "tabdo wincmd =",
 })
 
+-- Inlay hints
+-- vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   group = "LspAttach_inlayhints",
+--   callback = function(args)
+--     if not (args.data and args.data.client_id) then
+--       return
+--     end
+
+--     local bufnr = args.buf
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     require("lsp-inlayhints").on_attach(client, bufnr)
+--   end,
+-- })
+
 -- Fix semantic tokens for lsp
 autocmd("LspAttach", {
   callback = function(args)
