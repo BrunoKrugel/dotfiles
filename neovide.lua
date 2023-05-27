37250ccf1d1ab664b6f1030cc85871c527d2c2a4
Bruno Krugel <brunokrugel@gmail.com>
2023-05-27 20:34:07 -0300

    Update neovide.lua

--- neovide.lua
+++ neovide.lua
@@ -2,10 +2,10 @@ local opt = vim.opt
 local g = vim.g
 
 if g.neovide then
-    opt.guifont = "Hack Nerd Font:h12"
-    g.neovide_refresh_rate = 120
-    g.neovide_remember_window_size = true
-    g.neovide_cursor_antialiasing = true
-    g.neovide_input_macos_alt_is_meta = true
-    g.neovide_input_use_logo = false  
+  opt.guifont = "Hack Nerd Font:h12"
+  g.neovide_refresh_rate = 120
+  g.neovide_remember_window_size = true
+  g.neovide_cursor_antialiasing = true
+  g.neovide_input_macos_alt_is_meta = true
+  g.neovide_input_use_logo = false
 end
