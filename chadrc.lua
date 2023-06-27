c19c0a6d0e9a1aeeb4b5969006d22782a8211732
Bruno Krugel <brunokrugel@gmail.com>
2023-06-27 18:48:19 -0300

    move big things to core

--- chadrc.lua
+++ chadrc.lua
@@ -1,12 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 local fn = vim.fn
-
--- Clickable area
-_G.ClickMe = function()
-  print("Clicked!") 
-  -- require("noice").cmd("history")
-end
+local core = require "custom.configs.core"
 
 -- Path to overriding theme and highlights files
 local highlights = require "custom.highlights"
@@ -14,101 +9,7 @@ M.ui = {
   theme = "darcula",
   theme_toggle = { "darcula", "one_light" },
   lsp_semantic_tokens = false,
-  statusline = {
-    theme = "vscode_colored",
-    overriden_modules = function()
-      local st_modules = require "nvchad_ui.statusline.vscode_colored"
-      local modes = st_modules.modes
-
-      -- Load info for harpoon
-      local function get_marked()
-        local Marked = require "harpoon.mark"
-        local filename = vim.api.nvim_buf_get_name(0)
-        local success, index = pcall(Marked.get_index_of, filename)
-        if success and index and index ~= nil then
-          return "󱡀 " .. index .. " "
-        else
-          return ""
-        end
-      end
-      -- Load info for possession
-      -- local function get_session()
-      --   local session = require("nvim-possession").status()
-      --   if session ~= nil then
-      --     return "󰐃 "
-      --   else
-      --     return "󰐄 "
-      --   end
-      -- end
-
-      return {
-
-        -- St_NormalMode
-        -- St_VisualMode
-        -- St_InsertMode
-        -- St_TerminalMode
-        -- St_ReplaceMode
-        mode = function()
-          modes["n"][3] = "  "
-          modes["v"][3] = "  "
-          modes["i"][3] = "  "
-          modes["t"][3] = "  "
-          local m = vim.api.nvim_get_mode().mode
-          return "%#" .. modes[m][2] .. "#" .. (modes[m][3] or "  ") .. modes[m][1] .. " "
-        end,
-        fileInfo = function()
-          local icon = " 󰈚 "
-          local filename = fn.expand "%:t"
-          local icon_text
-
-          if filename ~= "Empty " then
-            local devicons_present, devicons = pcall(require, "nvim-web-devicons")
-
-            if devicons_present then
-              local ft_icon, ft_icon_hl = devicons.get_icon(filename, string.match(filename, "%a+$"))
-              icon = (ft_icon ~= nil and " " .. ft_icon) or ""
-              local icon_hl = ft_icon_hl or "DevIconDefault"
-              local hl_fg = fn.synIDattr(fn.synIDtrans(fn.hlID(icon_hl)), "fg")
-              local hl_bg = fn.synIDattr(fn.synIDtrans(fn.hlID "StText"), "bg")
-              vim.api.nvim_set_hl(0, "St_" .. icon_hl, { fg = hl_fg, bg = hl_bg })
-              icon_text = "%#St_" .. icon_hl .. "# " .. icon .. "%#StText# " .. filename .. " "
-            end
-          end
-
-          return icon_text or ("%#StText# " .. icon .. filename)
-        end,
-
-        LSP_status = function()
-          if rawget(vim, "lsp") then
-            for _, client in ipairs(vim.lsp.get_active_clients()) do
-              if client.attached_buffers[vim.api.nvim_get_current_buf()] and client.name ~= "null-ls" then
-                return (vim.o.columns > 100 and "%#St_LspStatus#   " .. client.name .. "  %#NotificationHl#%@v:lua.ClickMe@  ") or "   LSP  %#NotificationHl#%@v:lua.ClickMe@  "
-              end
-            end
-          end
-        end,
-
-        LSP_Diagnostics = function()
-          return "%#CopilotHl#"
-            .. require("copilot_status").status_string()
-            .. " "
-            .. "%#HarpoonHl#"
-            .. get_marked()
-            .. "%#BatteryHl#"
-            .. require("battery").get_status_line()
-            .. " "
-            -- .. "%#SessionHl#"
-            -- .. get_session()
-            -- .. " "
-            -- .. "%#NpmHl#"
-            -- .. get_npm()
-            -- .. " "
-            .. st_modules.LSP_Diagnostics()
-            -- .. "%#NotificationHl#%@v:lua.ClickMe@"
-        end,
-      }
-    end,
-  },
+  statusline = core.statusline,
 
   cmp = {
     icons = true,
@@ -128,80 +29,7 @@ M.ui = {
   hl_override = highlights.override,
   hl_add = highlights.add,
 
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      [[                                           ,o88888 ]],
-      [[                                        ,o8888888' ]],
-      [[                  ,:o:o:oooo.        ,8O88Pd8888"  ]],
-      [[              ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"    ]],
-      [[            ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"      ]],
-      [[           , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"        ]],
-      [[          , ..:.::o:ooOoOO8O888O8O,COCOO"          ]],
-      [[         , . ..:.::o:ooOoOOOO8OOOOCOCO"            ]],
-      [[          . ..:.::o:ooOoOoOO8O8OCCCC"o             ]],
-      [[             . ..:.::o:ooooOoCoCCC"o:o             ]],
-      [[             . ..:.::o:o:,cooooCo"oo:o:            ]],
-      [[          `   . . ..:.:cocoooo"'o:o:::'            ]],
-      [[          .`   . ..::ccccoc"'o:o:o:::'             ]],
-      [[         :.:.    ,c:cccc"':.:.:.:.:.'              ]],
-      [[       ..:.:"'`::::c:"'..:.:.:.:.:.'               ]],
-      [[     ...:.'.:.::::"'    . . . . .'                 ]],
-      [[    .. . ....:."' `   .  . . ''                    ]],
-      [[  . . . ...."'                                     ]],
-      [[  .. . ."'                                         ]],
-      [[ . . .''                                           ]],
-      [[ ..''                                              ]],
-      [[                                                  ]],
-      -- [[                @@@%%%%%%%%%@@                ]],
-      -- [[              @@@%%%%%%%%%#######%@@          ]],
-      -- [[            @@@@%%%%%%%######?######%@        ]],
-      -- [[            @@@@%%%%%%%#######:########%@     ]],
-      -- [[          @@@@@%%%%%%#########:??#######%     ]],
-      -- [[          @@@%%%%%####???###?+:??####?###@    ]],
-      -- [[        @@@%%%%%%#?+???###?:+?##??###?##@     ]],
-      -- [[      @??%@%%%##????????++:;+?+????????#@     ]],
-      -- [[      #  ;?%#?+; ..::+?+ ::;++++++?+???#      ]],
-      -- [[      %  :?%;;;:  ....:#+ :;+++????+???@      ]],
-      -- [[      #;;+??+++:   ...;##: ;;;++???++?%       ]],
-      -- [[      %#%+::++?#+;:::;?##+ ;;;;++??++#        ]],
-      -- [[      %?% : :???+?++???######?+;;+??#         ]],
-      -- [[      @%# ; ;??;;+ ;???+;;:..::.:+?%          ]],
-      -- [[        @???;;?+;;;+ ;:;;......;;;#@          ]],
-      -- [[        %##?++?+++;+ ??% @%%@@@@              ]],
-      -- [[        @_:?_:+_:_:#%                         ]],
-      -- [[                                              ]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⢶⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠟⠁⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣴⣶⣿⣿⣿⣿⣿⣷⣾⣶⣶⢂⣴⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⡴⠋⠁⠀⠀⠀⢀⣴⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣋⠒⠁⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣡⡾⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣱⡦⠋⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀ ⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⢵⠿⠋⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠈⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⠀⢀⢴⣿⠟⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⡀⠀⠀⠀⠀⠀⠀⣠⡶⠶⣿⡍⣆⠗⢨⠄⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⠀⠀⠀⠀⣀⣌⠻⡷⠮⠻⡇⣏⣰⡥⠄⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⡟⠉⠁⣀⠀⢀⣼⣿⠟⠁⠀⢃⠀⢡⠞⠛⣠⣶⣠⡀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⣰⣿⣿⢻⣿⠏⡴⠀⣰⠏⠀⠀⠚⠙⠛⣿⡿⢿⡀⠂⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀]],
-      -- [[⠀⠀⠀⣿⣿⣏⢟⡩⢹⣿⣟⡿⣋⣴⣷⠀⠀⠸⣻⠏⠐⡟⢘⣁⡼⠃⠀⠀⠀⠀⠤⠗⠛⠓⠒⠛⠂⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⠀⠀⠀]],
-      -- [[⠀⠀⠀⣿⣿⣿⣷⢬⣿⠇⣩⣾⣿⣿⣿⡖⠀⠀⠁⠀⠀⣿⡵⠛⠀⣠⣄⣀⡀⠀⠀⠀⢘⡽⠁⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣗⠀⠀⠀]],
-      -- [[⠀⠀⠀⣿⣿⣿⣷⡛⠁⣰⣿⣿⣿⣿⣿⣿⡃⠀⠀⠀⢀⣫⣥⠀⣴⣿⣿⣿⠿⠿⠟⠀⢸⡃⠀⡅⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀]],
-      -- [[⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣤⣄⣤⣿⡿⠟⠘⠛⠉⠉⠀⢀⠀⡀⡆⢹⣽⡈⠀⣠⣾⠇⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣶⠤⠀⠀⣀⠾⠼⠲⠃⠉⢏⠃⠀⠀⣿⣿⡃⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡏⡅⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠃⠀⠀⠹⠉⠁⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⣀⣤⣶⣾⣧⣀⡿⠀⠀⠀⠀⠀⣆⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⡿⢻⣿⡟⠀⠀⠀ ⠀]],
-      -- [[⠀⠀⠀ ⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠃⠀⠀⠰⠂⠀⠆⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠻⣿⣷⣿⡟⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠘⠟⠀⠀⠠⠠⠀⠀⠀⠀⠂⠀⠀⠀⠀⠀⢻⣭⣿⣿⣧⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠿⣿⣿⣿⣟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      -- [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⠿⢿⠿⠷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-    },
-  },
+  nvdash = core.nvdash,
 }
 
 M.lazy_nvim = {
