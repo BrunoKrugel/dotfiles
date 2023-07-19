f39407ca42f8494224c0a7eb12b5d6a35b9b3f5d
Bruno Krugel <brunokrugel@gmail.com>
2023-07-19 12:33:17 -0300

    add new null-ls sources

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -7,65 +7,54 @@ end
 local b = null_ls.builtins
 
 local sources = {
-  -- b.formatting.deno_fmt.with {
-  --   extra_args = {
-  --     "--config",
-  --     vim.fn.expand "~/.config/.deno.json",
-  --   },
-  -- },
+
+  -- Formatting
   b.formatting.prettier,
+  b.formatting.yamlfmt,
   b.formatting.stylua.with {
     extra_args = { "--config-path", vim.fn.expand "~/.config/stylua.toml" },
   },
-  -- Go
-  -- b.formatting.gofmt,
   b.formatting.gofumpt,
   b.formatting.tidy,
-  -- b.formatting.goimports,
+  b.formatting.goimports,
+  -- b.formatting.gofmt,
   -- b.formatting.goimports_reviser,
   -- b.diagnostics.golangci_lint,
   -- b.diagnostics.revive,
-  -- b.formatting.golines.with({
-  --   extra_args = {
-  --     "--max-len=180",
-  --     "--base-formatter=gofumpt",
-  --   },
-  -- }),
   -- b.formatting.lua_format,
-  --  b.diagnostics.luacheck,
+
+  -- Diagnostics
+  b.diagnostics.fish,
+  b.diagnostics.checkmake,
+  b.diagnostics.jsonlint,
+  b.diagnostics.staticcheck,
+  -- b.diagnostics.eslint,
+  -- b.diagnostics.luacheck,
   -- b.diagnostics.checkstyle.with {
   --   extra_args = { "-c", "/google_checks.xml" },
   -- },
-  b.formatting.yamlfmt,
-  -- b.formatting.xmlformat,
-  -- Diagnostics
-  -- b.diagnostics.eslint,
-  b.diagnostics.fish,
-  b.diagnostics.checkmake,
-  -- b.diagnostics.codespell,
-  b.diagnostics.jsonlint,
+
   -- Completion
   b.completion.luasnip,
+
   -- Code actions
-  -- b.code_actions.gitsigns,
+  b.code_actions.gitsigns.with {
+    config = {
+      filter_actions = function(title)
+        return title:lower():match "blame" == nil -- filter out blame actions
+      end,
+    },
+  },
   b.code_actions.gomodifytags,
+  b.code_actions.impl,
   b.code_actions.eslint,
+  b.code_actions.refactoring,
 }
 
--- local goimports = b.formatting.goimports
--- local e = os.getenv "GOIMPORTS_LOCAL"
--- if e ~= nil then
---   goimports = goimports.with { extra_args = { "-local", e } }
--- end
--- table.insert(sources, goimports)
-
--- for go.nvim
--- local gotest = require("go.null_ls").gotest()
--- local gotest_codeaction = require("go.null_ls").gotest_action()
--- local golangci_lint = require("go.null_ls").golangci_lint()
--- table.insert(sources, gotest)
--- table.insert(sources, golangci_lint)
--- table.insert(sources, gotest_codeaction)
+-- From go.nvim
+-- table.insert(sources, require("go.null_ls").golangci_lint())
+table.insert(sources, require("go.null_ls").gotest())
+table.insert(sources, require("go.null_ls").gotest_action())
 
 -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
