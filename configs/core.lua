9121c5bf758a251a8f1f366ebd4411e3a5c6d4cd
Bruno Krugel <brunokrugel@gmail.com>
2023-07-26 14:58:22 -0300

    new override syntax

--- configs/core.lua
+++ configs/core.lua
@@ -2,6 +2,17 @@ local M = {}
 
 require "custom.configs.mouse"
 
+function Get_marked()
+  local Marked = require "harpoon.mark"
+  local filename = vim.api.nvim_buf_get_name(0)
+  local success, index = pcall(Marked.get_index_of, filename)
+  if success and index and index ~= nil then
+    return "󱡀 " .. index .. " "
+  else
+    return ""
+  end
+end
+
 M.dapui = {
   icons = { expanded = "▾", collapsed = "▸" },
   mappings = {
@@ -57,80 +68,77 @@ M.tabufline = {
 
 M.statusline = {
   theme = "vscode_colored",
-  overriden_modules = function()
-    local st_modules = require "nvchad_ui.statusline.vscode_colored"
-    local modes = st_modules.modes
 
-    -- Load info for harpoon
-    local function get_marked()
-      local Marked = require "harpoon.mark"
-      local filename = vim.api.nvim_buf_get_name(0)
-      local success, index = pcall(Marked.get_index_of, filename)
-      if success and index and index ~= nil then
-        return "󱡀 " .. index .. " "
-      else
-        return ""
+  overriden_modules = function(modules)
+    modules[1] = (function()
+      local st_modules = require "nvchad_ui.statusline.vscode_colored"
+      local modes = st_modules.modes
+      modes["n"][3] = "  "
+      modes["v"][3] = "  "
+      modes["i"][3] = "  "
+      modes["t"][3] = "  "
+      local m = vim.api.nvim_get_mode().mode
+      return "%#" .. modes[m][2] .. "#" .. (modes[m][3] or "  ") .. modes[m][1] .. " "
+    end)()
+
+    modules[12] = (function()
+      if rawget(vim, "lsp") then
+        for _, client in ipairs(vim.lsp.get_active_clients()) do
+          if
+            client.attached_buffers[vim.api.nvim_get_current_buf()]
+            and (client.name ~= "null-ls" and client.name ~= "copilot")
+          then
+            return (vim.o.columns > 100 and "%#St_LspStatus#   " .. client.name) or "   LSP"
+          end
+        end
       end
-    end
+    end)()
 
-    return {
-      mode = function()
-        modes["n"][3] = "  "
-        modes["v"][3] = "  "
-        modes["i"][3] = "  "
-        modes["t"][3] = "  "
-        local m = vim.api.nvim_get_mode().mode
-        return "%#" .. modes[m][2] .. "#" .. (modes[m][3] or "  ") .. modes[m][1] .. " "
-      end,
-      fileInfo = function()
-        local icon = " 󰈚 "
-        local filename = vim.fn.expand "%:t"
-        local icon_text
+    modules[2] = (function()
+      local icon = " 󰈚 "
+      local filename = vim.fn.expand "%:t"
+      local icon_text
 
-        if filename ~= "Empty " then
-          local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+      if filename ~= "Empty " then
+        local devicons_present, devicons = pcall(require, "nvim-web-devicons")
 
-          if devicons_present then
-            local ft_icon, ft_icon_hl = devicons.get_icon(filename, string.match(filename, "%a+$"))
-            icon = (ft_icon ~= nil and " " .. ft_icon) or ""
-            local icon_hl = ft_icon_hl or "DevIconDefault"
-            local hl_fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(icon_hl)), "fg")
-            local hl_bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID "StText"), "bg")
-            vim.api.nvim_set_hl(0, "St_" .. icon_hl, { fg = hl_fg, bg = hl_bg })
-            icon_text = "%#St_" .. icon_hl .. "# " .. icon .. "%#StText# " .. filename .. " "
-          end
+        if devicons_present then
+          local ft_icon, ft_icon_hl = devicons.get_icon(filename, string.match(filename, "%a+$"))
+          icon = (ft_icon ~= nil and " " .. ft_icon) or ""
+          local icon_hl = ft_icon_hl or "DevIconDefault"
+          local hl_fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(icon_hl)), "fg")
+          local hl_bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID "StText"), "bg")
+          vim.api.nvim_set_hl(0, "St_" .. icon_hl, { fg = hl_fg, bg = hl_bg })
+          icon_text = "%#St_" .. icon_hl .. "# " .. icon .. "%#StText# " .. filename .. " "
         end
+      end
 
-        return icon_text or ("%#StText# " .. icon .. filename)
-      end,
+      return icon_text or ("%#StText# " .. icon .. filename)
+    end)()
 
-      LSP_status = function()
-        if rawget(vim, "lsp") then
-          for _, client in ipairs(vim.lsp.get_active_clients()) do
-            if
-              client.attached_buffers[vim.api.nvim_get_current_buf()]
-              and (client.name ~= "null-ls" and client.name ~= "copilot")
-            then
-              return (vim.o.columns > 100 and "%#St_LspStatus#   " .. client.name) or "   LSP"
-            end
-          end
-        end
-      end,
+    table.insert(
+      modules,
+      4,
+      (function()
+        return "%#CopilotHl#" .. require("copilot_status").status_string() .. " " .. "%#HarpoonHl#" .. Get_marked()
+      end)()
+    )
 
-      LSP_Diagnostics = function()
-        return "%#CopilotHl#"
-          .. require("copilot_status").status_string()
-          .. " "
-          .. "%#HarpoonHl#"
-          .. get_marked()
-          .. st_modules.LSP_Diagnostics()
-          .. "%#TermHl#%@v:lua.ClickTerm@  "
-      end,
+    table.insert(
+      modules,
+      6,
+      (function()
+        return "%#TermHl#%@v:lua.ClickTerm@  "
+      end)()
+    )
 
-      cwd = function()
-        return " %#NotificationHl#%@v:lua.ClickMe@  " .. st_modules.cwd()
-      end,
-    }
+    table.insert(
+      modules,
+      15,
+      (function()
+        return "  %#NotificationHl#%@v:lua.ClickMe@  "
+      end)()
+    )
   end,
 }
 
