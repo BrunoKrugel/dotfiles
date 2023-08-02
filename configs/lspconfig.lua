9a358d25ec6ae9ef4c9dd9bf853b86d2ce286fb0
Bruno Krugel <brunokrugel@gmail.com>
2023-08-02 15:27:44 -0300

    show all messages in lsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -151,11 +151,6 @@ vim.diagnostic.config {
   virtual_text = {
     source = "always",
     prefix = "■",
-    -- Only show virtual text matching the given severity
-    severity = {
-      -- Specify a range of severities
-      min = vim.diagnostic.severity.ERROR,
-    },
   },
   -- virtual_text = false,
   float = {
