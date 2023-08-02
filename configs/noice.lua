7d87bdf9191dc92cab01e29396a850628427b7b6
Bruno Krugel <brunokrugel@gmail.com>
2023-08-02 14:36:23 -0300

    Improve noice patterns

--- configs/noice.lua
+++ configs/noice.lua
@@ -7,8 +7,12 @@ end
 noice.setup {
   cmdline = {
     format = {
-      search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
-      search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
+      cmdline = { pattern = "^:", icon = "󰘳 ", lang = "vim" },
+      search_down = { kind = "search", pattern = "^/", icon = "󰩊 ", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = "󰩊 ", lang = "regex" },
+      filter = { pattern = "^:%s*!", icon = "󰻿 ", lang = "bash" },
+      lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+      help = { pattern = "^:%s*he?l?p?%s+", icon = "󰞋 " },
     },
   },
   routes = {
