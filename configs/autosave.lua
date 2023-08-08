8d30cb1e2c0f312e6fb66fa9078425b7f633029d
Bruno Krugel <brunokrugel@gmail.com>
2023-08-08 17:50:24 -0300

    Update autosave.lua

--- configs/autosave.lua
+++ configs/autosave.lua
@@ -15,6 +15,30 @@ autoSave.setup {
   },
   events = { "InsertLeave", "TextChanged" },
   silent = true,
+  callbacks = {
+    before_saving = function()
+      -- save global autoformat status
+      vim.g.OLD_AUTOFORMAT = vim.g.autoformat_enabled
+
+      vim.g.autoformat_enabled = false
+      vim.g.OLD_AUTOFORMAT_BUFFERS = {}
+      -- disable all manually enabled buffers
+      for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.b[bufnr].autoformat_enabled then
+          table.insert(vim.g.OLD_BUFFER_AUTOFORMATS, bufnr)
+          vim.b[bufnr].autoformat_enabled = false
+        end
+      end
+    end,
+    after_saving = function()
+      -- restore global autoformat status
+      vim.g.autoformat_enabled = vim.g.OLD_AUTOFORMAT
+      -- reenable all manually enabled buffers
+      for _, bufnr in ipairs(vim.g.OLD_AUTOFORMAT_BUFFERS or {}) do
+        vim.b[bufnr].autoformat_enabled = true
+      end
+    end,
+  },
   save_fn = function()
     if save_cmd ~= nil then
       vim.cmd(save_cmd)
