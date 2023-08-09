7fa4f3be3c9f289a6282ba81442f6d6758f1c2d7
Bruno Krugel <brunokrugel@gmail.com>
2023-08-09 13:33:00 -0300

    rename nvchad

--- mappings.lua
+++ mappings.lua
@@ -149,7 +149,7 @@ M.text = {
     ["<A-d>"] = { "<cmd>:MCstart<CR>", "Multi cursor" },
     ["<leader>ra"] = {
       function()
-        require("nvchad_ui.renamer").open()
+        require("nvchad.renamer").open()
       end,
       "󰑕 LSP rename",
     },
@@ -228,7 +228,7 @@ M.general = {
     },
     ["<leader>w"] = {
       function()
-        require("nvchad_ui.tabufline").close_buffer()
+        require("nvchad.tabufline").close_buffer()
       end,
       " Close buffer",
     },
@@ -361,14 +361,14 @@ M.tabufline = {
     -- cycle through buffers
     ["<tab>"] = {
       function()
-        require("nvchad_ui.tabufline").tabuflineNext()
+        require("nvchad.tabufline").tabuflineNext()
       end,
       " Goto next buffer",
     },
 
     ["<S-tab>"] = {
       function()
-        require("nvchad_ui.tabufline").tabuflinePrev()
+        require("nvchad.tabufline").tabuflinePrev()
       end,
       " Goto prev buffer",
     },
@@ -376,7 +376,7 @@ M.tabufline = {
     -- close buffer + hide terminal buffer
     ["<C-x>"] = {
       function()
-        require("nvchad_ui.tabufline").close_buffer()
+        require("nvchad.tabufline").close_buffer()
       end,
       " Close buffer",
     },
