33fb51ed4e2f1fda967680ad3cd25d3a1fcca3fe
Bruno Krugel <brunokrugel@gmail.com>
2023-08-09 13:33:17 -0300

    revert changes in statusline

--- configs/core.lua
+++ configs/core.lua
@@ -85,79 +85,79 @@ M.tabufline = {
 M.statusline = {
   theme = "vscode_colored",
 
-  -- overriden_modules = function(modules)
-  --   modules[1] = (function()
-  --     local st_modules = require "nvchad_ui.statusline.vscode_colored"
-  --     local modes = st_modules.modes
-  --     modes["n"][3] = "  "
-  --     modes["v"][3] = "  "
-  --     modes["i"][3] = "  "
-  --     modes["t"][3] = "  "
-  --     local m = vim.api.nvim_get_mode().mode
-  --     return "%#" .. modes[m][2] .. "#" .. (modes[m][3] or "  ") .. modes[m][1] .. " "
-  --   end)()
+  overriden_modules = function(modules)
+    modules[1] = (function()
+      local st_modules = require "nvchad.statusline.vscode_colored"
+      local modes = st_modules.modes
+      modes["n"][3] = "  "
+      modes["v"][3] = "  "
+      modes["i"][3] = "  "
+      modes["t"][3] = "  "
+      local m = vim.api.nvim_get_mode().mode
+      return "%#" .. modes[m][2] .. "#" .. (modes[m][3] or "  ") .. modes[m][1] .. " "
+    end)()
 
-    -- modules[2] = (function()
-    --   local icon = " 󰈚 "
-    --   local filename = vim.fn.expand "%:t"
-    --   local icon_text
+    modules[2] = (function()
+      local icon = " 󰈚 "
+      local filename = vim.fn.expand "%:t"
+      local icon_text
 
-    --   if filename ~= "Empty " then
-    --     local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+      if filename ~= "Empty " then
+        local devicons_present, devicons = pcall(require, "nvim-web-devicons")
 
-    --     if devicons_present then
-    --       local ft_icon, ft_icon_hl = devicons.get_icon(filename, string.match(filename, "%a+$"))
-    --       icon = (ft_icon ~= nil and " " .. ft_icon) or ""
-    --       local icon_hl = ft_icon_hl or "DevIconDefault"
-    --       local hl_fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(icon_hl)), "fg")
-    --       local hl_bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID "StText"), "bg")
-    --       vim.api.nvim_set_hl(0, "St_" .. icon_hl, { fg = hl_fg, bg = hl_bg })
-    --       icon_text = "%#St_" .. icon_hl .. "# " .. icon .. "%#StText# " .. filename .. " "
-    --     end
-    --   end
+        if devicons_present then
+          local ft_icon, ft_icon_hl = devicons.get_icon(filename, string.match(filename, "%a+$"))
+          icon = (ft_icon ~= nil and " " .. ft_icon) or ""
+          local icon_hl = ft_icon_hl or "DevIconDefault"
+          local hl_fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(icon_hl)), "fg")
+          local hl_bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID "StText"), "bg")
+          vim.api.nvim_set_hl(0, "St_" .. icon_hl, { fg = hl_fg, bg = hl_bg })
+          icon_text = "%#St_" .. icon_hl .. "# " .. icon .. "%#StText# " .. filename .. " "
+        end
+      end
 
-    --   return icon_text or ("%#StText# " .. icon .. filename)
-    -- end)()
+      return icon_text or ("%#StText# " .. icon .. filename)
+    end)()
 
-    -- table.insert(
-    --   modules,
-    --   4,
-    --   (function()
-    --     return "%#HarpoonHl#" .. Get_marked() .. Get_record()
-    --   end)()
-    -- )
+    table.insert(
+      modules,
+      4,
+      (function()
+        return "%#HarpoonHl#" .. Get_marked() .. Get_record()
+      end)()
+    )
 
-    -- table.insert(
-    --   modules,
-    --   6,
-    --   (function()
-    --     return "%#TermHl#%@v:lua.ClickTerm@  "
-    --   end)()
-    -- )
+    table.insert(
+      modules,
+      6,
+      (function()
+        return "%#TermHl#%@v:lua.ClickTerm@  "
+      end)()
+    )
 
-    -- modules[14] = (function()
-    --   if rawget(vim, "lsp") then
-    --     for _, client in ipairs(vim.lsp.get_active_clients()) do
-    --       if
-    --         client.attached_buffers[vim.api.nvim_get_current_buf()]
-    --         and (client.name ~= "null-ls" and client.name ~= "copilot")
-    --       then
-    --         local copilot = "%#CopilotHl#" .. require("copilot_status").status_string() .. " "
-    --         return (vim.o.columns > 100 and copilot .. "%#St_LspStatus# " .. client.name) or copilot .. "  LSP"
-    --       end
-    --     end
-    --   end
-    --   return ""
-    -- end)()
+    modules[14] = (function()
+      if rawget(vim, "lsp") then
+        for _, client in ipairs(vim.lsp.get_active_clients()) do
+          if
+            client.attached_buffers[vim.api.nvim_get_current_buf()]
+            and (client.name ~= "null-ls" and client.name ~= "copilot")
+          then
+            local copilot = "%#CopilotHl#" .. require("copilot_status").status_string() .. " "
+            return (vim.o.columns > 100 and copilot .. "%#St_LspStatus# " .. client.name) or copilot .. "  LSP"
+          end
+        end
+      end
+      return ""
+    end)()
 
-    -- table.insert(
-    --   modules,
-    --   15,
-    --   (function()
-    --     return " %#NotificationHl#%@v:lua.ClickMe@  " .. require("recorder").recordingStatus()
-    --   end)()
-    -- )
-  -- end,
+    table.insert(
+      modules,
+      15,
+      (function()
+        return " %#NotificationHl#%@v:lua.ClickMe@  " .. require("recorder").recordingStatus()
+      end)()
+    )
+  end,
 }
 
 M.nvdash = {
