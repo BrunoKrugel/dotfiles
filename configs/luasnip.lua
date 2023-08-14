3033e2412ab70daa046290af572b9d35331078bb
Bruno Krugel <brunokrugel@gmail.com>
2023-08-14 00:39:06 -0300

    luasnip

--- configs/luasnip.lua
+++ configs/luasnip.lua
@@ -8,6 +8,7 @@ luasnip.filetype_extend("javascriptreact", { "html" })
 luasnip.filetype_extend("vue", { "html" })
 
 require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/custom/luasnip" })
 
 local types = require "luasnip.util.types"
 
