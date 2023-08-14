a9a26735d506fc9bf6d46821e25b5b9378e33b32
Bruno Krugel <brunokrugel@gmail.com>
2023-08-13 23:59:09 -0300

    neotest adjust

---
+++ configs/neotest.lua
@@ -0,0 +1,79 @@
+local neotest_ns = vim.api.nvim_create_namespace "neotest"
+local border = require "custom.utils.border"
+
+vim.diagnostic.config({
+  virtual_text = {
+    format = function(diagnostic)
+      local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+      return message
+    end,
+  },
+}, neotest_ns)
+
+require("neotest").setup {
+  adapters = {
+    require "neotest-go" {
+      args = { "-count=1", "-coverprofile coverage.out", "-covermode=count" },
+    },
+    require "neotest-jest" {
+      jestCommand = "npm test --",
+      env = { CI = true },
+      cwd = function(path)
+        return vim.fn.getcwd()
+      end,
+    },
+  },
+  diagnostic = {
+    enabled = false,
+  },
+  floating = {
+    border = "rounded",
+    max_height = 0.6,
+    max_width = 0.6,
+  },
+  icons = {
+    child_indent = "│",
+    child_prefix = "├",
+    collapsed = "─",
+    expanded = "╮",
+    failed = "✖",
+    final_child_indent = " ",
+    final_child_prefix = "╰",
+    non_collapsible = "─",
+    passed = "",
+    running = "",
+    skipped = "",
+    unknown = "",
+  },
+  output = {
+    enabled = true,
+    open_on_run = true,
+  },
+  run = {
+    enabled = true,
+  },
+  status = {
+    enabled = true,
+  },
+  strategies = {
+    integrated = {
+      height = 40,
+      width = 120,
+    },
+  },
+  summary = {
+    enabled = true,
+    expand_errors = true,
+    follow = true,
+    mappings = {
+      attach = "a",
+      expand = { "<CR>", "<2-LeftMouse>" },
+      expand_all = "e",
+      jumpto = "i",
+      output = "o",
+      run = "r",
+      short = "O",
+      stop = "u",
+    },
+  },
+}
--- plugins.lua
+++ plugins.lua
@@ -818,24 +818,10 @@ local plugins = {
     dependencies = {
       "antoinemadec/FixCursorHold.nvim",
       "nvim-neotest/neotest-go",
+      "haydenmeade/neotest-jest",
     },
     config = function()
-      local neotest_ns = vim.api.nvim_create_namespace "neotest"
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
-      require("neotest").setup {
-        adapters = {
-          require "neotest-go" {
-            args = { "-count=1", "-coverprofile coverage.out", "-covermode=count" },
-          },
-        },
-      }
+      require "custom.configs.neotest"
     end,
   },
   {
