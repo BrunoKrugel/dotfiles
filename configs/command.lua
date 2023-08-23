2b3c1c1617fb657e8c75b02e2e9d9e8a89ce6f20
Bruno Krugel <brunokrugel@gmail.com>
2023-08-23 12:45:15 -0300

    Add reload window

--- configs/command.lua
+++ configs/command.lua
@@ -1,8 +1,12 @@
-require("telescope").load_extension("command_center")
+require("telescope").load_extension "command_center"
 
-require("command_center").add({
+require("command_center").add {
   {
     desc = "Replace",
     cmd = "<CMD>MurenToggle<CR>",
   },
-})
+  {
+    desc = "Reload Window",
+    cmd = "<CMD>windo e<CR>zz",
+  },
+}
