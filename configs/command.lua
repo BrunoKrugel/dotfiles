43593636c6aca3b35a075331ca1f97dc57894bc4
Bruno Krugel <brunokrugel@gmail.com>
2023-08-24 14:43:08 -0300

    Improve command

--- configs/command.lua
+++ configs/command.lua
@@ -1,12 +1,46 @@
-require("telescope").load_extension "command_center"
+local telescope = require "telescope"
+local command_center = require "command_center"
 
 require("command_center").add {
   {
-    desc = "Replace",
+    desc = " Replace",
     cmd = "<CMD>MurenToggle<CR>",
+    category = "text",
   },
   {
-    desc = "Reload Window",
-    cmd = "<CMD>windo e<CR>zz",
+    desc = "󰑓 Reload Window",
+    cmd = "<CMD>windo e<CR>",
+    category = "ui",
+  },
+  {
+    desc = " Open Folders",
+    cmd = "<CMD>UFOOpen<CR>",
+    category = "text",
+  },
+    {
+    desc = " Pick Color",
+    cmd = "<CMD>Colortils picker<CR>",
+    category = "web",
+  },
+
+
+
+}
+
+telescope.setup {
+  extensions = {
+    command_center = {
+      components = {
+        command_center.component.DESColortils picker
+        
+      },
+      sort_by = {
+        command_center.component.DESC,
+        command_center.component.KEYS,
+      },
+      auto_replace_desc_with_cmd = false,
+    },
   },
 }
+
+telescope.load_extension "command_center"
