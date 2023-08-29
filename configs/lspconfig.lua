20527251d3b841e47f7bc2c6fd2a1deeb46a5c20
Bruno Krugel <brunokrugel@gmail.com>
2023-08-29 11:57:49 -0300

    make luals faster

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -66,6 +66,16 @@ require("mason-lspconfig").setup_handlers {
           telemetry = {
             enable = false,
           },
+          workspace = {
+            library = {
+              [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+              [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+              [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
+              [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+            },
+            maxPreload = 100000,
+            preloadFileSize = 10000,
+          },
         },
       },
     }
