cf82be2916b76b74b016bbf51c93f0968a3dc06d
Bruno Krugel <brunokrugel@gmail.com>
2023-08-29 23:28:35 -0300

    DapIcons

--- init.lua
+++ init.lua
@@ -7,7 +7,8 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 
 vim.fn.sign_define("DapBreakpoint", { text = "", numhl = "DapBreakpoint", texthl = "DapBreakpoint" })
 vim.fn.sign_define("DagLogPoint", { text = "", numhl = "DapLogPoint", texthl = "DapLogPoint" })
-vim.fn.sign_define("DapStopped", { text = "", numhl = "DapStopped", texthl = "DapStopped" })
+vim.fn.sign_define("DapStopped", { text = "", numhl = "DapStopped", texthl = "DapStopped" })
+vim.fn.sign_define("DapBreakpointRejected", { text = "", numhl = "DapBreakpointRejected", texthl = "DapBreakpointRejected" })
 
 vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
 
