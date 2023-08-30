63d34905557442a6d4953f14d8e3cbd6c6b7796c
Bruno Krugel <brunokrugel@gmail.com>
2023-08-29 23:37:34 -0300

    use correct signs

--- configs/trouble.lua
+++ configs/trouble.lua
@@ -42,13 +42,5 @@ trouble.setup {
   auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
   auto_fold = false, -- automatically fold a file trouble list at creation
   auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
-  signs = {
-    -- icons / text used for a diagnostic
-    error = "",
-    warning = "",
-    hint = "",
-    information = "",
-    other = "",
-  },
-  use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+  use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
 }
