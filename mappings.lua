d7c0266bc816d6016afdc1007eddb0ed099acae6
Bruno Krugel <brunokrugel@gmail.com>
2023-09-05 14:56:02 -0300

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -12,48 +12,6 @@ local M = {}
 -- <kPlus> -> Keypad Plus (+)
 -- <kMinus> -> Keypad Minus (-)
 
-local escape_code = vim.api.nvim_replace_termcodes("<Esc>", false, false, true)
-local backspace_code = vim.api.nvim_replace_termcodes("<BS>", false, false, true)
-local function viml_backspace()
-  vim.cmd [[
-        let g:exprvalue =
-        \ (&indentexpr isnot '' ? &indentkeys : &cinkeys) =~? '!\^F' &&
-        \ &backspace =~? '.*eol\&.*start\&.*indent\&' &&
-        \ !search('\S','nbW',line('.')) ? (col('.') != 1 ? "\<C-U>" : "") .
-        \ "\<bs>" . (getline(line('.')-1) =~ '\S' ? "" : "\<C-F>") : "\<bs>"
-        ]]
-  return vim.g.exprvalue
-end
-local function backspace()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
-  if ft == "TelescopePrompt" then
-    return vim.api.nvim_feedkeys(backspace_code, "n", true)
-  end
-
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local before_cursor_is_whitespace = vim.api.nvim_get_current_line():sub(0, col):match "^%s*$"
-
-  if not before_cursor_is_whitespace then
-    return require("nvim-autopairs").autopairs_bs()
-  end
-  if line == 1 then
-    return viml_backspace()
-  end
-  local correct_indent = require("nvim-treesitter.indent").get_indent(line)
-  local current_indent = vim.fn.indent(line)
-  local previous_line_is_whitespace = vim.api.nvim_buf_get_lines(0, line - 2, line - 1, false)[1]:match "^%s*$"
-  if current_indent == correct_indent then
-    if previous_line_is_whitespace then
-      return viml_backspace()
-    end
-    return backspace_code
-  elseif current_indent > correct_indent then
-    return escape_code .. "==0wi"
-  end
-  return backspace_code
-end
-
 M.disabled = {
   n = {
     ["<leader>b"] = "",
@@ -154,14 +112,6 @@ M.text = {
 
     -- ["<C-Down>"] = { "<CMD> :m+<CR>", "󰜯 Move line down" },
     ["<C-Down>"] = { "<CMD>m .+1<CR>==", "󰜯 Move line down" },
-
-    ["<BS>"] = {
-      function()
-        backspace()
-      end,
-      "Smart backspace",
-      opts = { expr = true, noremap = true, replace_keycodes = false },
-    },
 
     -- Navigate
     ["<A-Left>"] = { "<ESC>I", " Move to beginning of line" },
