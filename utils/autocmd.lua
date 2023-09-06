02999c67afa59a5c7484847f064f7a0ca879f64f
Bruno Krugel <brunokrugel@gmail.com>
2023-09-06 07:10:41 -0300

    Update autocmd.lua

--- utils/autocmd.lua
+++ utils/autocmd.lua
@@ -341,6 +341,25 @@ autocmd("BufHidden", {
   end,
 })
 
+autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    local api = require "nvim-tree.api"
+    -- Only 1 window with nvim-tree left: we probably closed a file buffer
+    if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
+      -- Required to let the close event complete. An error is thrown without this.
+      vim.defer_fn(function()
+        -- close nvim-tree: will go to the last hidden buffer used before closing
+        api.tree.toggle { find_file = true, focus = true }
+        -- re-open nivm-tree
+        api.tree.toggle { find_file = true, focus = true }
+        -- nvim-tree is still the active window. Go to the previous window.
+        vim.cmd "wincmd p"
+      end, 0)
+    end
+  end,
+})
+
 -- Open NvimTree on startup
 -- autocmd("VimEnter", {
 --   callback = function()
