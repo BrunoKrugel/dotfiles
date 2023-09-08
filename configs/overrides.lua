aaf52c2dfde5b9fce610df87a4cdbd87a666b0e8
Bruno Krugel <brunokrugel@gmail.com>
2023-09-08 02:50:06 -0300

    clean telescope

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -325,10 +325,10 @@ M.telescope = {
   },
   extensions = {
     fzf = {
-      fuzzy = true,
       override_generic_sorter = true,
       override_file_sorter = true,
       case_mode = "smart_case",
+      fuzzy = true,
     },
     ast_grep = {
       command = {
@@ -338,18 +338,6 @@ M.telescope = {
       },
       grep_open_files = false,
       lang = nil,
-    },
-    lazy = {
-      show_icon = true,
-      mappings = {
-        open_in_browser = "<C-o>",
-        open_in_file_browser = "<M-b>",
-        open_in_find_files = "<C-f>",
-        open_in_live_grep = "<C-g>",
-        open_plugins_picker = "<C-b>",
-        open_lazy_root_find_files = "<C-r>f",
-        open_lazy_root_live_grep = "<C-r>g",
-      },
     },
   },
 }
