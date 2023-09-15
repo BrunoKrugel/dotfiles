96d2e28caf75eac13e6eee9ab42900898a5fe8e9
Bruno Krugel <brunokrugel@gmail.com>
2023-09-15 16:14:48 -0300

    toggleterm with edgy

--- plugins.lua
+++ plugins.lua
@@ -812,27 +812,93 @@ local plugins = {
     config = true,
   },
   {
-    "akinsho/toggleterm.nvim",
-    keys = { [[<C-\>]] },
-    cmd = { "ToggleTerm", "ToggleTermOpenAll", "ToggleTermCloseAll" },
-    dependencies = {
-      {
-        "folke/edgy.nvim",
-        init = function()
-          vim.opt.laststatus = 3
-          vim.opt.splitkeep = "screen"
-        end,
-        opts = {
-          bottom = {
-            {
-              ft = "toggleterm",
-              size = { height = 0.2 },
-            },
-          },
-          wo = {
-            winbar = false,
-          },
+    "folke/edgy.nvim",
+    event = "BufReadPost",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    opts = {
+      bottom = {
+        {
+          ft = "toggleterm",
+          size = { height = 0.2 },
         },
+        { ft = "spectre_panel", size = { height = 0.4 } },
+        { ft = "qf", title = "QuickFix" },
+        { ft = "dapui_watches", title = "Watches" },
+        { ft = "dap-repl", title = "Debug REPL" },
+        { ft = "dapui_console", title = "Debug Console" },
+        "Trouble",
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- only show help buffers
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        {
+          ft = "NoiceHistory",
+          title = " Log",
+          open = function()
+            toggle_noice()
+          end,
+        },
+        {
+          ft = "neotest-output-panel",
+          title = " Test Output",
+          open = function()
+            vim.cmd.vsplit()
+            require("neotest").output_panel.toggle()
+          end,
+        },
+        {
+          ft = "DiffviewFileHistory",
+          title = " Diffs",
+        },
+      },
+      left = {
+        { ft = "undotree", title = "Undo Tree" },
+        { ft = "dapui_scopes", title = "Scopes" },
+        { ft = "dapui_breakpoints", title = "Breakpoints" },
+        { ft = "dapui_stacks", title = "Stacks" },
+        {
+          ft = "diff",
+          title = " Diffs",
+        },
+
+        {
+          ft = "DiffviewFileHistory",
+          title = " Diffs",
+        },
+        {
+          ft = "DiffviewFiles",
+          title = " Diffs",
+        },
+        {
+          ft = "neotest-summary",
+          title = "  Tests",
+          open = function()
+            require("neotest").summary.toggle()
+          end,
+        },
+      },
+      right = {
+        "dapui_scopes",
+        "sagaoutline",
+        "neotest-output-panel",
+        "neotest-summary",
+      },
+      options = {
+        left = { size = 40 },
+        bottom = { size = 20 },
+        right = { size = 30 },
+        top = { size = 10 },
+      },
+      wo = {
+        winbar = false,
+        signcolumn = "no",
       },
     },
   },
