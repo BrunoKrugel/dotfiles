544cb4dff7fd46d4f002f839bb2af8a83084fc10
Bruno Krugel <brunokrugel@gmail.com>
2023-09-16 02:03:58 -0300

    stop lazy notify

--- chadrc.lua
+++ chadrc.lua
@@ -16,6 +16,8 @@ M.ui = {
     icons = true,
     lspkind_text = true,
     style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "colored", -- colored / simple
   },
 
   lsp = {
@@ -57,6 +59,8 @@ M.settings = {
     "help",
   },
 }
+
+M.lazy_nvim = core.lazy
 
 M.gitsigns = {
   signs = {
