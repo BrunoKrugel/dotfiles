b8c587923e0318013e887744821134d446e472bd
Bruno Krugel <brunokrugel@gmail.com>
2023-09-17 19:17:03 -0300

    format

--- utils/options.lua
+++ utils/options.lua
@@ -6,47 +6,32 @@ vim.wo.statuscolumn = ""
 
 opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
-opt.backup = false
+opt.backup = false --- Recommended by coc
 opt.swapfile = false
 opt.scrolloff = 10 -- always show minimum n lines after current line
 opt.relativenumber = false -- Show relative numberline
 opt.wrap = false
 opt.iskeyword:append "-"
 opt.termguicolors = true -- True color support
+opt.autoindent = true --- Good auto indent
+opt.backspace = "indent,eol,start" --- Making sure backspace works
 
 -- Map in dotyfile
 g.mapleader = " "
 g.maplocalleader = " "
 
--- Folding settings
--- zc fold block
--- zo unfold block
--- zM fold all blocks
--- zR unfold all blocks
--- za toggle folding
-opt.foldenable = true
-
--- UFO
-opt.foldcolumn = "1" -- show foldcolumn in nvim 0.9
--- opt.foldlevel = 1
 opt.conceallevel = 2
--- opt.foldnestmax = 1
--- opt.foldlevelstart = 99
-opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+-- Folds
+opt.foldenable = true
+opt.foldcolumn = "auto" -- show foldcolumn in nvim 0.9
+opt.foldnestmax = 0
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+-- opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+opt.fillchars = [[stl: ,eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 -- opt.foldmethod = "expr"
-
--- CUSTOM
--- vim.o.foldmethod = "expr"
 -- opt.foldexpr = "nvim_treesitter#foldexpr()"
--- vim.o.signcolumn = "yes"
--- vim.o.foldcolumn = "2"
--- vim.o.signcolumn = "number"
-o.number = true
-o.numberwidth = 2
-
--- Spell
--- opt.spell = true
--- opt.spelllang = { 'en_us' }
 
 -- Copilot
 g.copilot_assume_mapped = true
@@ -55,10 +40,9 @@ g.copilot_assume_mapped = true
 g.bookmark_sign = ""
 g.bookmark_highlight = "CopilotHl"
 
-o.statuscolumn = "%=%l%s%C"
-o.emoji = false
-o.cursorline = true
-o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+opt.emoji = false
+opt.cursorline = true
+opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 if g.neovide then
   -- opt.guifont = "Hack Nerd Font:h12"
