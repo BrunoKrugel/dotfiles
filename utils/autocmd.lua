5ac947330a677882d3dabbda1111679a1faf271b
Bruno Krugel <brunokrugel@gmail.com>
2023-09-17 19:16:52 -0300

    format

--- utils/autocmd.lua
+++ utils/autocmd.lua
@@ -371,21 +371,21 @@ autocmd("BufEnter", {
 -- Triger `autoread` when files changes on disk
 -- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
 -- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
   group = group,
   command = [[silent! if mode() != 'c' && !bufexists("[Command Line]") | checktime | endif]],
 })
 
 -- Notification after file change
 -- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
-vim.api.nvim_create_autocmd("FileChangedShellPost", {
+autocmd("FileChangedShellPost", {
   group = group,
   command = [[echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None]],
 })
 
 -- Hide cursorline in insert mode
-vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, { command = "set cursorline", group = group })
-vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, { command = "set nocursorline", group = group })
+autocmd({ "InsertLeave", "WinEnter" }, { command = "set cursorline", group = group })
+autocmd({ "InsertEnter", "WinLeave" }, { command = "set nocursorline", group = group })
 
 -- local lazy_did_show_install_view = false
 
