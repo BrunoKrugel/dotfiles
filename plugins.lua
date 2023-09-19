fa0b3629e86b3c9c09dd7c2a4c511bf408762c8d
Bruno Krugel <brunokrugel@gmail.com>
2023-09-19 15:58:59 -0300

    use a session plugin that dont mess with my folds

--- plugins.lua
+++ plugins.lua
@@ -351,31 +351,30 @@ local plugins = {
     },
   },
   {
-    "rmagatti/auto-session",
+    "olimorris/persisted.nvim",
     event = "VimEnter",
-    config = function()
-      require "custom.configs.session"
-    end,
+    opts = {
+      save_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/sessions/"), -- directory where session files are saved
+      silent = true, -- silent nvim message when sourcing session file
+      use_git_branch = true, -- create session files based on the branch of the git enabled repository
+      autosave = true, -- automatically save session files when exiting Neovim
+      should_autosave = nil, -- function to determine if a session should be autosaved
+      autoload = true, -- automatically load the session for the cwd on Neovim startup
+      on_autoload_no_session = nil, -- function to run when `autoload = true` but there is no session to load
+      follow_cwd = true, -- change session file name to match current working directory if it changes
+      allowed_dirs = nil, -- table of dirs that the plugin will auto-save and auto-load from
+      ignored_dirs = nil, -- table of dirs that are ignored when auto-saving and auto-loading
+      telescope = { -- options for the telescope extension
+        reset_prompt_after_deletion = true, -- whether to reset prompt after session deleted
+      },
+      config = function()
+        vim.o.sessionoptions = "buffers,curdir,folds,globals,tabpages,winpos,winsize"
+        require("persisted").setup(opts)
+        require("telescope").load_extension("persisted")
+      end,
+    },
   },
   { "b0o/schemastore.nvim" },
-  -- {
-  --   "ahmedkhalf/project.nvim",
-  --   opts = {
-  --     manual_mode = false,
-  --     detection_methods = { "pattern", "lsp" },
-  --     patterns = { ".git", ".vscode", ".svn", "Makefile", "package.json" },
-  --     ignore_lsp = {},
-  --     exclude_dirs = {},
-  --     show_hidden = false,
-  --     silent_chdir = true,
-  --     scope_chdir = "global",
-  --     datapath = vim.fn.stdpath "data",
-  --   },
-  --   event = "VeryLazy",
-  --   config = function(_, opts)
-  --     require("project_nvim").setup(opts)
-  --   end,
-  -- },
   {
     "ThePrimeagen/harpoon",
     cmd = "Harpoon",
@@ -541,7 +540,6 @@ local plugins = {
   },
   {
     "lukas-reineke/virt-column.nvim",
-    cmd = "CCToggle",
     config = function()
       require("virt-column").setup {
         char = "┃",
