9a60794752b7d85af6413f26ddfe43ce5f98f330
Bruno Krugel <brunokrugel@gmail.com>
2023-09-22 23:46:21 -0300

    usercmd to disable diagnostics

--- utils/usercmd.lua
+++ utils/usercmd.lua
@@ -48,6 +48,34 @@ create_cmd("TPeek", function()
   require("peek").open()
 end, {})
 
+-- Command to toggle inline diagnostics
+vim.api.nvim_create_user_command(
+  'DiagnosticsToggleVirtualText',
+  function()
+    local current_value = vim.diagnostic.config().virtual_text
+    if current_value then
+      vim.diagnostic.config({virtual_text = false})
+    else
+      vim.diagnostic.config({virtual_text = true})
+    end
+  end,
+  {}
+)
+
+-- Command to toggle diagnostics
+vim.api.nvim_create_user_command(
+  'DiagnosticsToggle',
+  function()
+    local current_value = vim.diagnostic.is_disabled()
+    if current_value then
+      vim.diagnostic.enable()
+    else
+      vim.diagnostic.disable()
+    end
+  end,
+  {}
+)
+
 -- Toggle colorcolumn
 create_cmd("TCC", function()
   vim.g.ccenable = not vim.g.ccenable
