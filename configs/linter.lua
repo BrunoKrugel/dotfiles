d8af55f536837dd3c3ff424ff6951b3c20014e1e
Bruno Krugel <brunokrugel@gmail.com>
2023-11-07 01:01:44 -0300

    Create linter.lua

---
+++ configs/linter.lua
@@ -0,0 +1,31 @@
+local present, lint = pcall(require, "linter")
+
+if not present then
+  return
+end
+
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+  group = lint_augroup,
+  callback = function()
+    lint.try_lint()
+  end,
+})
+
+lint.linters_by_ft = {
+  lua = {
+    "luacheck",
+  },
+  yaml = {
+    "yamllint",
+    "actionlint",
+  },
+  go = {
+    "golangcilint",
+  },
+  codespell = { "codespell" },
+  javascript = { "eslint_d" },
+  typescript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
+}
