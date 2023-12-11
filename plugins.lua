d641fc60ed5a6b99a527c08c21e2eb4412a9f8f9
Bruno Krugel <brunokrugel@gmail.com>
2023-12-11 14:55:56 -0300

    remove deprecated plugin

--- plugins.lua
+++ plugins.lua
@@ -687,19 +687,6 @@ local plugins = {
     end,
   },
   {
-    "weilbith/nvim-code-action-menu",
-    cmd = "CodeActionMenu",
-    init = function()
-      vim.g.code_action_menu_show_details = true
-      vim.g.code_action_menu_show_diff = true
-      vim.g.code_action_menu_show_action_kind = true
-    end,
-    config = function()
-      dofile(vim.g.base46_cache .. "git")
-      dofile(vim.g.base46_cache .. "codeactionmenu")
-    end,
-  },
-  {
     "kdheepak/lazygit.nvim",
     cmd = "LazyGit",
   },
