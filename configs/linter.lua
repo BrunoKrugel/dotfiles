f52fff5dadd2e1ff81022d77ccbd24a7ac962239
Bruno Krugel <brunokrugel@gmail.com>
2023-12-11 14:49:11 -0300

    linter events

--- configs/linter.lua
+++ configs/linter.lua
@@ -5,7 +5,7 @@ if not present then
 end
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   group = lint_augroup,
   callback = function()
     lint.try_lint()
