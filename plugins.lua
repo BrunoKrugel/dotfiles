6898b539277f84d08c89a0468c8ab487203878ba
Bruno Krugel <brunokrugel@gmail.com>
2023-12-11 14:56:16 -0300

    notify message

--- plugins.lua
+++ plugins.lua
@@ -607,7 +607,25 @@ local plugins = {
     event = "VeryLazy",
     dependencies = {
       "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+      {
+        "rcarriga/nvim-notify",
+        config = function(_, opts)
+          local job = require "plenary.job"
+          job
+            :new({
+              command = "curl",
+              args = { "https://vtip.43z.one" },
+              on_exit = function(j, exit_code)
+                local res = table.concat(j:result())
+                if exit_code ~= 0 then
+                  res = "Error fetching tip: " .. res
+                end
+                require "notify"(res)
+              end,
+            })
+            :start()
+        end,
+      },
     },
     config = function()
       require "custom.configs.noice"
