f973eceb6b648c4acd7d5b8b7df4b1bd9614d3bc
Bruno Krugel <brunokrugel@gmail.com>
2024-02-27 14:26:23 -0300

    filter noice

--- configs/noice.lua
+++ configs/noice.lua
@@ -79,6 +79,14 @@ noice.setup {
           { find = "%d+ lines yanked" },
           { find = "^Hunk %d+ of %d+$" },
           { find = "%d+L, %d+B$" },
+          { find = "^[/?].*" }, -- Searching up/down
+          { find = "E486: Pattern not found:" }, -- Searcingh not found
+          { find = "%d+ changes?;" }, -- Undoing/redoing
+          { find = "%d+ fewer lines" }, -- Deleting multiple lines
+          { find = "%d+ more lines" }, -- Undoing deletion of multiple lines
+          { find = "%d+ lines " }, -- Performing some other verb on multiple lines
+          { find = "Already at newest change" }, -- Redoing
+          { find = '"[^"]+" %d+L, %d+B' }, -- Saving
 
           -- Save
           { find = " bytes written" },
@@ -103,7 +111,7 @@ noice.setup {
           { find = "1 line less" },
 
           -- General messages
-          { find = "Already at newest change" },
+          { find = "Already at newest change" }, -- Redoing
           { find = "Already at oldest change" },
           { find = "E21: Cannot make changes, 'modifiable' is off" },
         },
