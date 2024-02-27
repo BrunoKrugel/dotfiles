b8d43010f95b3e75739df0120437d462ad0dd847
Bruno Krugel <brunokrugel@gmail.com>
2024-02-27 14:01:53 -0300

    yank message

--- utils/usercmd.lua
+++ utils/usercmd.lua
@@ -33,6 +33,27 @@ create_cmd("YankPathFull", function()
   yank_file_path "%:~"
 end, { desc = "Yank current file's absolute path" })
 
+local function getLatestMessages(count)
+  local messages = vim.fn.execute "messages"
+  local lines = vim.split(messages, "\n")
+  lines = vim.tbl_filter(function(line)
+    return line ~= ""
+  end, lines)
+  count = count and tonumber(count) or nil
+  count = (count ~= nil and count > 0) and count - 1 or #lines
+  return table.concat(vim.list_slice(lines, #lines - count), "\n")
+end
+
+local function yankMessages(register, count)
+  local default_register = "+"
+  register = (register and register ~= "") and register or default_register
+  vim.fn.setreg(register, getLatestMessages(count), "l")
+end
+
+create_cmd("YankMessages", function(opts)
+  return yankMessages(opts.reg, opts.count)
+end, { count = 10, register = true })
+
 create_cmd("DapUiOpen", ":lua require'dapui'.open()", {})
 create_cmd("DapUiClose", ":lua require'dapui'.close()", {})
 create_cmd("DapUiToggle", ":lua require'dapui'.toggle()", {})
@@ -77,7 +98,16 @@ create_cmd("Format", function(args)
       ["end"] = { args.line2, end_line:len() },
     }
   end
-  require("conform").format { async = true, lsp_fallback = true, range = range }
+
+  local opts = {
+    bufnr = args.buf,
+    timeout_ms = 1000,
+    async = false,
+    quiet = false,
+    lsp_fallback = true,
+    range = range,
+  }
+  require("conform").format(opts)
 end, { range = true })
 
 -- Toggle colorcolumn
