5ea9b6147e639d76a7f98361a24e36c7f44e1a5e
Bruno Krugel <brunokrugel@gmail.com>
2024-06-16 15:51:47 -0300

    fix trouble

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -754,7 +754,26 @@ return {
     ft = { "qf" },
     cmd = "Trouble",
     config = function()
-      require("trouble").setup {}
+      require("trouble").setup {
+        modes = {
+          mydiags = {
+            mode = "diagnostics", -- inherit from diagnostics mode
+            filter = {
+              any = {
+                buf = 0, -- current buffer
+                {
+                  severity = vim.diagnostic.severity.ERROR, -- errors only
+                  -- limit to files in the current project
+                  function(item)
+                    return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+                  end,
+                },
+              },
+            },
+          },
+        },
+        auto_close = true,
+      }
       dofile(vim.g.base46_cache .. "trouble")
     end,
   },
