466f13a7534bdf5d9750bb4254a65d8bbb0e8f83
Bruno Krugel <brunokrugel@gmail.com>
2024-07-20 13:12:54 -0300

    surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -115,7 +115,21 @@ return {
   {
     "NStefan002/visual-surround.nvim",
     event = "BufEnter",
-    opts = true,
+    config = function()
+      require("visual-surround").setup {
+        use_default_keymaps = false,
+        exit_visual_mode = false,
+      }
+      local preffered_mapping_prefix = "s"
+      local surround_chars = { "{", "[", "(", "'", '"', "<" }
+      local surround = require("visual-surround").surround
+
+      for _, key in pairs(surround_chars) do
+        vim.keymap.set("v", "s" .. key, function()
+          surround(key)
+        end, { desc = "[visual-surround] Surround selection with " .. key })
+      end
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
