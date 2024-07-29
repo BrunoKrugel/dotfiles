1c5b0bb6b2724cb11bd891d23252222ea1598409
Bruno Krugel <brunokrugel@gmail.com>
2024-07-29 12:23:14 -0300

    conform http format and Go fix

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,19 +1,40 @@
-local prettier = { "prettier" }
+local conform = require "conform"
 
-require("conform").setup {
+---@param bufnr integer
+---@param ... string
+---@return string
+local function first(bufnr, ...)
+  for i = 1, select("#", ...) do
+    local formatter = select(i, ...)
+    if conform.get_formatter_info(formatter, bufnr).available then
+      return formatter
+    end
+  end
+  return select(1, ...)
+end
+
+conform.setup {
   notify_on_error = false,
+  kulala = {
+    command = "kulala-fmt",
+    args = { "$FILENAME" },
+    stdin = false,
+  },
   formatters_by_ft = {
-    javascript = { prettier },
-    typescript = { prettier },
-    javascriptreact = { prettier },
-    typescriptreact = { prettier },
-    json = { prettier },
-    jsonc = { prettier },
-    css = { prettier },
-    html = { prettier },
-    markdown = { prettier },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    json = { "prettier" },
+    jsonc = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    markdown = { "prettier" },
     lua = { "stylua" },
-    go = { "goimports", "gofumpt" },
+    http = { "kulala" },
+    go = function(bufnr)
+      return { first(bufnr, "goimports", "gofumpt") }
+    end,
     ["vue"] = { "prettier" },
     ["scss"] = { "prettier" },
     ["less"] = { "prettier" },
