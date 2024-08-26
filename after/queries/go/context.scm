a0aadd6d4ff3b9d910c52bdddcde77e3bc259647
Bruno Krugel <brunokrugel@gmail.com>
2024-08-26 17:04:09 -0300

    cleanup go

--- after/queries/go/context.scm
+++ after/queries/go/context.scm
@@ -1,12 +1,3 @@
-;((function_declaration
-;		name: (identifier) @context_name))
-;
-;((method_declaration
-;        name: (field_identifier) @context_name))
-;
-;((type_declaration
-;  (type_spec
-;    name: (type_identifier) @context_name)))
 (function_declaration) @scope
 
 (method_declaration) @scope
