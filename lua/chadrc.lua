c4fcdcfe30fcdbe1b8b7ef4b42fa9ea5a0611516
Bruno Krugel <brunokrugel@gmail.com>
2024-09-13 10:14:42 -0300

    format tailwind cmp

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -13,6 +13,10 @@ M.ui = {
   cmp = {
     icons = true,
     lspkind_text = true,
+    format_colors = {
+      tailwind = true,
+      icon = "󱓻",
+    },
     style = "default", -- default/flat_light/flat_dark/atom/atom_colored
     border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
     selected_item_bg = "colored", -- colored / simple
