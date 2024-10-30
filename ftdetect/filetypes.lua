7671e9398479956485d545cbc8fa204d9fb1eab1
Bruno Krugel <brunokrugel@gmail.com>
2024-10-29 23:51:57 -0300

    Add bigfiles

--- ftdetect/filetypes.lua
+++ ftdetect/filetypes.lua
@@ -1,10 +1,24 @@
-vim.filetype.add({
-    pattern = {
-      [".*%.scm"] = "query"
-    },
-    extension = {
-      ['http'] = 'http',
-      ['scm'] = 'query',
-      ['query'] = 'query',
-    },
-  })
+vim.filetype.add {
+  pattern = {
+    [".*%.scm"] = "query",
+    ["tsconfig*.json"] = "jsonc",
+    [".*/%.vscode/.*%.json"] = "jsonc",
+    -- Borrowed from LazyVim. Mark huge files to disable features later.
+    [".*"] = function(path, bufnr)
+      return vim.bo[bufnr]
+          and vim.bo[bufnr].filetype ~= "bigfile"
+          and path
+          and vim.fn.getfsize(path) > (1024 * 500) -- 500 KB
+          and "bigfile"
+        or nil
+    end,
+  },
+  filename = {
+    [".eslintrc.json"] = "jsonc",
+  },
+  extension = {
+    ["http"] = "http",
+    ["scm"] = "query",
+    ["query"] = "query",
+  },
+}
