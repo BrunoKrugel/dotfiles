d87b8b179cc88a65a5831abc5fcb5651640f16cc
Bruno Krugel <brunokrugel@gmail.com>
2024-11-13 20:42:35 -0300

    Obsidian

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -434,8 +434,58 @@ return {
       require("obsidian").setup {
         dir = "/users/bruno.krugel/Library/Mobile Documents/iCloud~md~obsidian/Documents/Annotation",
         disable_frontmatter = true,
+        workspaces = {
+          {
+            name = "Annotation",
+            path = "~/users/bruno.krugel/Library/Mobile Documents/iCloud~md~obsidian/Documents/Annotation",
+          },
+        },
         completion = {
           nvim_cmp = true,
+          min_chars = 2,
+        },
+        -- Optional, customize how markdown links are formatted.
+        ---@param opts {path: string, label: string, id: string|?}
+        ---@return string
+        markdown_link_func = function(opts)
+          return string.format("[%s](%s)", opts.label, opts.path)
+        end,
+        ui = {
+          enable = true, -- set to false to disable all additional syntax features
+          update_debounce = 200, -- update delay after a text change (in milliseconds)
+          -- Define how various check-boxes are displayed
+          checkboxes = {
+            -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "", hl_group = "ObsidianDone" },
+            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+            -- Replace the above with this if you don't have a patched font:
+            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+            -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+            -- You can also add more custom ones...
+          },
+          -- Use bullet marks for non-checkbox lists.
+          bullets = { char = "•", hl_group = "ObsidianBullet" },
+          external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          -- Replace the above with this if you don't have a patched font:
+          -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          reference_text = { hl_group = "ObsidianRefText" },
+          highlight_text = { hl_group = "ObsidianHighlightText" },
+          tags = { hl_group = "ObsidianTag" },
+          hl_groups = {
+            -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+            ObsidianTodo = { bold = true, fg = "#f78c6c" },
+            ObsidianDone = { bold = true, fg = "#89ddff" },
+            ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+            ObsidianTilde = { bold = true, fg = "#ff5370" },
+            ObsidianBullet = { bold = true, fg = "#89ddff" },
+            ObsidianRefText = { underline = true, fg = "#c792ea" },
+            ObsidianExtLinkIcon = { fg = "#c792ea" },
+            ObsidianTag = { italic = true, fg = "#89ddff" },
+            ObsidianHighlightText = { bg = "#75662e" },
+          },
         },
       }
     end,
