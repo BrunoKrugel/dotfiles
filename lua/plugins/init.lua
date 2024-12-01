5185d00311fbddca6f043e88ceab199a729682e3
Bruno Krugel <brunokrugel@gmail.com>
2024-12-01 13:55:33 -0300

    Cursor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -125,6 +125,17 @@ return {
     enabled = false,
   },
   {
+    "sphamba/smear-cursor.nvim",
+    lazy = false,
+    opts = {
+      stiffness = 0.8,
+      trailing_stiffness = 0.6,
+      trailing_exponent = 0,
+      distance_stop_animating = 0.5,
+      hide_target_hack = false,
+    },
+  },
+  {
     "NStefan002/visual-surround.nvim",
     event = "BufEnter",
     config = function()
