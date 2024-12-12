5aed9297cc1f032e08cde078299ee931428200dc
Bruno Krugel <brunokrugel@gmail.com>
2024-12-12 14:57:47 -0300

    go.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1097,8 +1097,31 @@ return {
         },
       },
     },
-    config = function()
-      require "configs.go"
+    opts = {
+      lsp_inlay_hints = {
+        enable = false,
+        other_hints_prefix = "•",
+      },
+      trouble = true,
+      lsp_keymaps = false,
+      diagnostic = false,
+      lsp_codelens = true,
+      floaterm = {
+        posititon = "auto",
+        width = 0.45,
+        height = 0.98,
+        title_colors = "dracula",
+      },
+      icons = { breakpoint = "", currentpos = "" },
+      gocoverage_sign = "│",
+      -- dap_debug_gui = core.dapui,
+      dap_debug_gui = false,
+    },
+    config = function(_, opts)
+      require("go").setup(ops)
+      vim.api.nvim_set_hl(0, "goCoverageUncover", { fg = "#f9e2af" })
+      vim.api.nvim_set_hl(0, "goCoverageUncovered", { fg = "#F38BA8" })
+      vim.api.nvim_set_hl(0, "goCoverageCovered", { fg = "#a6e3a1" })
     end,
     build = ':lua require("go.install").update_all_sync()',
   },
