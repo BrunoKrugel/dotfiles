7bd8879a1ccda06bdb23eac2ade00317b46b0cdd
Bruno Krugel <brunokrugel@gmail.com>
2024-12-30 15:34:04 -0300

    Todo icon

--- lua/configs/todo.lua
+++ lua/configs/todo.lua
@@ -3,7 +3,7 @@ require("todo-comments").setup {
   sign_priority = 8,
   keywords = {
     FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
-    TODO = { icon = " ", color = "info" },
+    TODO = { icon = " ", color = "info" },
     HACK = { icon = " ", color = "warning" },
     WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
     PERF = { icon = "󰑮 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
