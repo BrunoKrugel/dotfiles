24639021b498fa0cb1eadad96a1f7b27800f3054
Bruno Krugel <brunokrugel@gmail.com>
2025-01-12 20:48:56 -0300

    Cleanup

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,33 +2,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true, buffer = 0 }
 
-local function golang_goto_def()
-  local old = vim.lsp.buf.definition
-  local opts = {
-    on_list = function(options)
-      if options == nil or options.items == nil or #options.items == 0 then
-        return
-      end
-      local targetFile = options.items[1].filename
-      local prefix = string.match(targetFile, "(.-)_templ%.go$")
-      if prefix then
-        local function_name = vim.fn.expand "<cword>"
-        options.items[1].filename = prefix .. ".templ"
-        vim.fn.setqflist({}, " ", options)
-        vim.api.nvim_command "cfirst"
-        vim.api.nvim_command("silent! /templ " .. function_name)
-      else
-        old()
-      end
-    end,
-  }
-  vim.lsp.buf.definition = function(o)
-    o = o or {}
-    o = vim.tbl_extend("keep", o, opts)
-    old(o)
-  end
-end
-
 local function handle_copy()
   local mode = vim.fn.mode()
   if mode == "v" or mode == "V" or mode == "" then
