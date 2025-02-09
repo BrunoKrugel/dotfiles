8ce7789fac3681ea70232ee892e6afedd91bf0b9
Bruno Krugel <brunokrugel@gmail.com>
2025-02-09 17:09:33 -0300

    Fix checkbox padding

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -367,6 +367,9 @@ return {
         dash = {
           width = 79,
         },
+        bullet = {
+          right_pad = 1,
+      },
         pipe_table = {
           style = "full", -- use 'normal' if colorcolumn is important for you.
         },
@@ -395,17 +398,12 @@ return {
         ui = {
           enable = true, -- set to false to disable all additional syntax features
           update_debounce = 200, -- update delay after a text change (in milliseconds)
-          -- Define how various check-boxes are displayed
           checkboxes = {
             -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
             [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
             ["x"] = { char = "", hl_group = "ObsidianDone" },
             [">"] = { char = "", hl_group = "ObsidianRightArrow" },
             ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-            -- Replace the above with this if you don't have a patched font:
-            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-            -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-
             -- You can also add more custom ones...
           },
           -- Use bullet marks for non-checkbox lists.
