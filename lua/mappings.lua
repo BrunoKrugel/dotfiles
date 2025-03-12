2632c5db8fb68085d5806dbee4f1041a9995f450
Bruno Krugel <brunokrugel@gmail.com>
2025-03-12 17:23:23 -0300

    Code actions

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -54,6 +54,72 @@ local function move_or_create_win(key)
   end
 end
 
+local code_actions = function()
+  local function apply_specific_code_action(res)
+    -- vim.notify(vim.inspect(res))
+    vim.lsp.buf.code_action {
+      filter = function(action)
+        return action.title == res.title
+      end,
+      apply = true,
+    }
+  end
+
+  local actions = {}
+
+  actions["Goto Definition"] = { priority = 100, call = vim.lsp.buf.definition }
+  actions["Goto Implementation"] = { priority = 200, call = vim.lsp.buf.implementation }
+  actions["Show References"] = { priority = 300, call = vim.lsp.buf.references }
+  actions["Rename"] = { priority = 400, call = vim.lsp.buf.rename }
+
+  local bufnr = vim.api.nvim_get_current_buf()
+  local params = vim.lsp.util.make_range_params()
+
+  params.context = {
+    triggerKind = vim.lsp.protocol.CodeActionTriggerKind.Invoked,
+    diagnostics = vim.lsp.diagnostic.get_line_diagnostics(),
+  }
+
+  vim.lsp.buf_request(bufnr, "textDocument/codeAction", params, function(_, results, _, _)
+    if not results or #results == 0 then
+      return
+    end
+    for i, res in ipairs(results) do
+      local prio = 10
+      if res.isPreferred then
+        if res.kind == "quickfix" then
+          prio = 0
+        else
+          prio = 1
+        end
+      end
+      actions[res.title] = {
+        priority = prio,
+        call = function()
+          apply_specific_code_action(res)
+        end,
+      }
+    end
+    local items = {}
+    for t, action in pairs(actions) do
+      table.insert(items, { title = t, priority = action.priority })
+    end
+    table.sort(items, function(a, b)
+      return a.priority < b.priority
+    end)
+    local titles = {}
+    for _, item in ipairs(items) do
+      table.insert(titles, item.title)
+    end
+    vim.ui.select(titles, {}, function(choice)
+      if choice == nil then
+        return
+      end
+      actions[choice].call()
+    end)
+  end)
+end
+
 local function is_diag_for_cur_pos()
   local diagnostics = vim.diagnostic.get(0)
   local pos = vim.api.nvim_win_get_cursor(0)
@@ -494,6 +560,10 @@ map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 map("n", "<C-n>", function()
   require("hover").hover_switch "next"
 end, { desc = "hover.nvim (next source)" })
+
+map({ "n", "i", "v" }, "<C-.>", function()
+  code_actions()
+end, { desc = "Code Action" })
 
 map("n", "<F12>", "<CMD>Glance references<CR>", { desc = "󰘐 References" })
 
