00d58e4db2f6d2ed121c7a60a19f6d0a66c23845
Bruno Krugel <brunokrugel@gmail.com>
2025-03-24 09:30:49 -0300

    Detach unused LSP

--- lua/custom/utils/autocmd.lua
+++ lua/custom/utils/autocmd.lua
@@ -562,3 +562,19 @@ autocmd({ "CursorHold", "CursorHoldI" }, {
     require("lint").try_lint()
   end,
 })
+
+autocmd({ "LspDetach" }, {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client or not client.attached_buffers then
+      return
+    end
+    for buf_id in pairs(client.attached_buffers) do
+      if buf_id ~= args.buf then
+        return
+      end
+    end
+    client:stop()
+  end,
+  desc = "Stop lsp client when no buffer is attached",
+})
