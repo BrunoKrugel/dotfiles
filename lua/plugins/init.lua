0628fad972229954b7b8459d02185a9171de24be
Bruno Krugel <brunokrugel@gmail.com>
2025-04-09 15:15:15 -0300

    cleanup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1015,13 +1015,6 @@ return {
       "typescriptreact",
     },
   },
-  -- {
-  --   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  --   event = "LspAttach",
-  --   config = function()
-  --     require("lsp_lines").setup()
-  --   end,
-  -- },
   {
     "nvim-neotest/neotest",
     ft = { "go", "javascript", "typescript", "javascriptreact", "typescriptreact" },
