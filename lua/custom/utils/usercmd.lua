29a6907265d4f859e585e61176da097379a273ce
Bruno Krugel <brunokrugel@gmail.com>
2025-05-13 13:45:13 -0300

    Unused cmd

--- lua/custom/utils/usercmd.lua
+++ lua/custom/utils/usercmd.lua
@@ -33,27 +33,6 @@ create_cmd("YankPathFull", function()
   yank_file_path "%:~"
 end, { desc = "Yank current file's absolute path" })
 
-local function getLatestMessages(count)
-  local messages = vim.fn.execute "messages"
-  local lines = vim.split(messages, "\n")
-  lines = vim.tbl_filter(function(line)
-    return line ~= ""
-  end, lines)
-  count = count and tonumber(count) or nil
-  count = (count ~= nil and count > 0) and count - 1 or #lines
-  return table.concat(vim.list_slice(lines, #lines - count), "\n")
-end
-
-local function yankMessages(register, count)
-  local default_register = "+"
-  register = (register and register ~= "") and register or default_register
-  vim.fn.setreg(register, getLatestMessages(count), "l")
-end
-
-create_cmd("YankMessages", function(opts)
-  return yankMessages(opts.reg, opts.count)
-end, { count = 10, register = true })
-
 create_cmd("DapUiOpen", ":lua require'dapui'.open()", {})
 create_cmd("DapUiClose", ":lua require'dapui'.close()", {})
 create_cmd("DapUiToggle", ":lua require'dapui'.toggle()", {})
@@ -61,17 +40,9 @@ create_cmd("DapUiFloatElement", ":lua require'dapui'.float_element()", {})
 create_cmd("DapUiWidget", ":lua require'dap.ui.widgets'.hover()", {})
 create_cmd("DapUiEval", ":lua require'dapui'.eval()", {})
 
-create_cmd("UFOOpen", function()
-  require("ufo").openAllFolds()
-end, {})
 
 create_cmd("Http", function()
   require("kulala").run()
-end, {})
-
---Open Peek
-create_cmd("TPeek", function()
-  require("peek").open()
 end, {})
 
 create_cmd("ToggleInlayHints", function()
