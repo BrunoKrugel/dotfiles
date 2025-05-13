af8ffe9744d63703f1deaae4a13f4ba0554739f7
Bruno Krugel <brunokrugel@gmail.com>
2025-05-13 13:04:24 -0300

    cleanup neotest

--- lua/configs/neotest.lua
+++ lua/configs/neotest.lua
@@ -1,14 +1,5 @@
 local neotest_ns = vim.api.nvim_create_namespace "neotest"
 
-vim.diagnostic.config({
-  virtual_text = {
-    format = function(diagnostic)
-      local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-      return message
-    end,
-  },
-}, neotest_ns)
-
 require("neotest").setup {
   adapters = {
     require "neotest-go" {
