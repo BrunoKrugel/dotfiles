3ef123961819278110bca03af459856cad98177d
Bruno Krugel <brunokrugel@gmail.com>
2025-11-03 13:04:20 -0300

    go tags

---
+++ after/queries/go/injections.scm
@@ -0,0 +1,11 @@
+(field_declaration
+  tag: (raw_string_literal
+    (raw_string_literal_content) @injection.content
+    (#set! injection.self)
+  ))
+
+(field_declaration
+  tag: (raw_string_literal
+    (raw_string_literal_content) @injection.content
+    (#set! injection.language "go_tags")
+  ))
